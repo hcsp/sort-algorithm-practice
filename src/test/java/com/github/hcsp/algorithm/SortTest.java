3af0e2c275a66d3aab52f93a8b5327a1450cd4b4
fcbhank <hankhu4work@163.com>
2019-09-23 23:22:45 +0800

    实现了冒泡排序和快速排序

--- src/test/java/com/github/hcsp/algorithm/SortTest.java
+++ src/test/java/com/github/hcsp/algorithm/SortTest.java
@@ -1,38 +1,44 @@
 
 package com.github.hcsp.algorithm;
-import org.junit.jupiter.api.Test;;import java.util.Random;;import java.util.Arrays;;import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
 
 public class SortTest {
-int[] randomArray = new int[10];
+    int[] randomArray = new int[10];
 
-{
-    Random r = new Random();
-    for (int i = 0; i < 10; i++) {
-        randomArray[i] = r.nextInt(10);
+    {
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            randomArray[i] = r.nextInt(10);
+        }
     }
-}
 
-@Test
-public void sort1Test() {
-    int[] copy = Arrays.copyOf(randomArray, 10);
-    Arrays.sort(copy);
-
-
-    System.out.println("Before: " + Arrays.toString(randomArray));
-    Sort.sort1(randomArray);`
-    System.out.println("After: " + Arrays.toString(randomArray));
-    Assertions.assertArrayEquals(copy, randomArray);
-}
-
-@Test
-public void sort2Test() {
-    int[] copy = Arrays.copyOf(randomArray, 10);
-    Arrays.sort(copy);
+    @Test
+    public void sort1Test() {
+        int[] copy = Arrays.copyOf(randomArray, 10);
+        Arrays.sort(copy);
 
 
-    System.out.println("Before: " + Arrays.toString(randomArray));
-    Sort.sort1(randomArray);
-    System.out.println("After: " + Arrays.toString(randomArray));
-    Assertions.assertArrayEquals(copy, randomArray);
-}
+        System.out.println("Before: " + Arrays.toString(randomArray));
+        Sort.sort1(randomArray);
+        System.out.println("After: " + Arrays.toString(randomArray));
+        Assertions.assertArrayEquals(copy, randomArray);
+    }
+
+    @Test
+    public void sort2Test() {
+        int[] copy = Arrays.copyOf(randomArray, 10);
+        Arrays.sort(copy);
+
+
+        System.out.println("Before: " + Arrays.toString(randomArray));
+        Sort.sort1(randomArray);
+        System.out.println("After: " + Arrays.toString(randomArray));
+        Assertions.assertArrayEquals(copy, randomArray);
+    }
 }
