a5ab32ecfa165dac93490e30fbaea7e371e53f8b
9vvvv9 <1466255084@qq.com>
2019-09-30 01:48:05 +0800

    第2次提交

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -6,18 +6,42 @@ public class Sort {
     public static void main(String[] args) {
         int[] array1 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
         int[] array2 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
-        sort1(array1);
-        sort2(array2);
+        selectSort(array1);
+        bubbleSort(array2);
 
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
     }
 
-    // 鎺掑簭绠楁硶1
-    // 鎸夌収浠庡皬鍒板ぇ鎺掑簭
-    public static void sort1(int[] array) {}
+    // 排序算法1
+    // 按照从小到大排序
+    public static void selectSort(int[] array) {
+    	for(int x=0;x<array.length-1;x++)
+    	{
+    		for(int y=x+1;y<array.length;y++)
+    		{
+    			if(array[x]>array[y])
+    			{
+    				int temp = array[x];
+    				array[x] = array[y];
+    				array[y] = temp;
+    			}
+    		}
+    	}
+    }
 
-    // 鎺掑簭绠楁硶2
-    // 鎸夌収浠庡皬鍒板ぇ鎺掑簭
-    public static void sort2(int[] array) {}
-}
+    // 排序算法2
+    // 按照从小到大排序
+    public static void bubbleSort(int[] array) {
+    	for(int x=0;x<array.length-1;x++)
+    	{
+    		for(int y=0;y<array.length-x-1;y++)
+    		{
+    			if(array[y]>array[y+1])
+    			{
+    				int temp = array[y];
+    				array[y] = array[y+1];
+    				array[y+1] = temp;
+    			}
+    		}
+    	}
