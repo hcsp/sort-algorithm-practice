1d798ecf5201bf516730df4250c70a38c5523f16
zhangyongwang <20225614@qq.com>
2019-09-29 10:58:37 +0800

    finish bubbleSort 、 insertSort

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -4,20 +4,41 @@ import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] array1 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
-        int[] array2 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
-        sort1(array1);
-        sort2(array2);
+        int[] array1 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array2 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        bubbleSort(array1);
+        insertSort(array2);
+
 
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
+
+
     }
 
-    // 排序算法1
-    // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    // 冒泡排序
+    public static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
 
-    // 排序算法2
-    // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    // 插入排序
+    public static void insertSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
 }
--- src/test/java/com/github/hcsp/algorithm/SortTest.java
+++ src/test/java/com/github/hcsp/algorithm/SortTest.java
@@ -21,7 +21,7 @@ public class SortTest {
         Arrays.sort(copy);
 
         System.out.println("Before: " + Arrays.toString(randomArray));
-        Sort.sort1(randomArray);
+        Sort.bubbleSort(randomArray);
         System.out.println("After: " + Arrays.toString(randomArray));
         Assertions.assertArrayEquals(copy, randomArray);
     }
@@ -32,7 +32,7 @@ public class SortTest {
         Arrays.sort(copy);
 
         System.out.println("Before: " + Arrays.toString(randomArray));
-        Sort.sort1(randomArray);
+        Sort.bubbleSort(randomArray);
         System.out.println("After: " + Arrays.toString(randomArray));
         Assertions.assertArrayEquals(copy, randomArray);
     }
