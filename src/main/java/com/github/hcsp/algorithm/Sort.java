e5613ec90a54f635c3f769db022006d9da706c7b
JinLingfeng <37367546+JinLingfeng@users.noreply.github.com>
2019-10-26 20:05:37 +0800

    quick sort and bubble sort (#19)
    quick sort and bubble sort (#19)

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -15,9 +15,51 @@ public class Sort {
 
     // 排序算法1
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+
+    public static void sort1(int[] array) {
+        quickSort(array, 0, array.length-1);
+    }
+    public static void quickSort(int[] array, int start, int end){
+        if (start >= end || array == null || array.length <= 1){
+            return;
+        }
+        int i = start;
+        int j = end;
+        int base = array[(start + end) / 2];
+        while (i <= j) {
+            while (array[i] < base) {
+                i++;
+            }
+            while (array[j] > base) {
+                j--;
+            }
+            if (i < j) {
+                int t = array[i];
+                array[i] = array[j];
+                array[j] = t;
+                ++i;
+                --j;
+            } else if (i == j) {
+                ++i;
+            }
+        }
+        quickSort(array, start, j);
+        quickSort(array, i, end);
+    }
 
     // 排序算法2
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    public static void sort2(int[] array) {
+        //Bubble sort
+        int temp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length-1; j++) {
+                if (array[j] > array [j+1]){
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
 }
