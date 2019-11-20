c042533a72e7f5ec55baf9298a17f9b04fe357fd
Bigbig Zheng <39644918+zhengbigbig@users.noreply.github.com>
2019-11-20 21:52:40 +0800

    算法排序 (#25)
    算法排序 (#25)

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -4,20 +4,90 @@ import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] array1 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
-        int[] array2 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array1 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array2 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
         sort1(array1);
         sort2(array2);
+        //        sort3(array2, 0, array2.length - 1);
+
 
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
     }
 
-    // 排序算法1
+    // 冒泡排序
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    public static void sort1(int[] array) {
+        int len = array.length;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
 
-    // 排序算法2
+    // 归并排序
+    private static void sort2(int[] arr) {
+        int len = arr.length;
+        int[] result = new int[len];
+        mergeSortRecursive(arr, result, 0, len - 1);
+    }
+
+    private static void mergeSortRecursive(int[] arr, int[] result, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int mid = (start + end) >> 1;
+        int start1 = start, end1 = mid;
+        int start2 = mid + 1, end2 = end;
+        mergeSortRecursive(arr, result, start1, end1);
+        mergeSortRecursive(arr, result, start2, end2);
+        int k = start;
+        while (start1 <= end1 && start2 <= end2) {
+            result[k++] = arr[start1] < arr[start2] ? arr[start1++] : arr[start2++];
+        }
+        while (start1 <= end1) {
+            result[k++] = arr[start1++];
+        }
+        while (start2 <= end2) {
+            result[k++] = arr[start2++];
+        }
+        for (k = start; k <= end; k++) {
+            arr[k] = result[k];
+        }
+
+    }
+
+    // 快速排序
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    private static void sort3(int[] array, int head, int tail) {
+        if (array == null || head >= tail || tail <= 1) {
+            return;
+        }
+        int i = head, j = tail, pivot = array[(head + tail) / 2];
+        while (i <= j) {
+            while (array[i] < pivot) {
+                ++i;
+            }
+            while (array[j] > pivot) {
+                --j;
+            }
+            if (i < j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                ++i;
+                --j;
+            } else if (i == j) {
+                ++i;
+            }
+        }
+        sort3(array, head, j);
+        sort3(array, i, tail);
+    }
+
 }
