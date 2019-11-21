2a276fb4278febe8cfed7397b21f30334f1401e0
zhengbigbig <780357902@qq.com>
2019-11-21 16:48:51 +0800

    算法排序:冒泡  快速  归并  插入  选择  堆

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -2,18 +2,31 @@ package com.github.hcsp.algorithm;
 
 import java.util.Arrays;
 
+
 public class Sort {
     public static void main(String[] args) {
         int[] array1 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
         int[] array2 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array3 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array4 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array5 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array6 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
         sort1(array1);
         sort2(array2);
-        //        sort3(array2, 0, array2.length - 1);
+        quickSort(array3, 0, array3.length - 1);
+        insertionSort(array4);
+        selectionSort(array5);
+        heapSort(array6);
 
 
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
+        System.out.println(Arrays.toString(array3));
+        System.out.println(Arrays.toString(array4));
+        System.out.println(Arrays.toString(array5));
+        System.out.println(Arrays.toString(array6));
     }
+
 
     // 冒泡排序
     // 按照从小到大排序
@@ -46,6 +59,7 @@ public class Sort {
         int start2 = mid + 1, end2 = end;
         mergeSortRecursive(arr, result, start1, end1);
         mergeSortRecursive(arr, result, start2, end2);
+
         int k = start;
         while (start1 <= end1 && start2 <= end2) {
             result[k++] = arr[start1] < arr[start2] ? arr[start1++] : arr[start2++];
@@ -56,6 +70,7 @@ public class Sort {
         while (start2 <= end2) {
             result[k++] = arr[start2++];
         }
+
         for (k = start; k <= end; k++) {
             arr[k] = result[k];
         }
@@ -64,7 +79,7 @@ public class Sort {
 
     // 快速排序
     // 按照从小到大排序
-    private static void sort3(int[] array, int head, int tail) {
+    private static void quickSort(int[] array, int head, int tail) {
         if (array == null || head >= tail || tail <= 1) {
             return;
         }
@@ -86,8 +101,78 @@ public class Sort {
                 ++i;
             }
         }
-        sort3(array, head, j);
-        sort3(array, i, tail);
+        quickSort(array, head, j);
+        quickSort(array, i, tail);
     }
+
+    // 插入
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int tg = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > tg) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = tg;
+        }
+    }
+
+    // 选择排序
+    private static void selectionSort(int[] arr) {
+        int len = arr.length;
+        if (len <= 0) {
+            return;
+        }
+        for (int i = 0; i < len - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < len; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    // 堆排序
+    private static void heapSort(int[] arr) {
+        // 1.数组堆化，从最后一个叶子节点开始
+        int len = arr.length;
+        int beginIndex = (len >> 1) - 1;
+        for (int i = beginIndex; i >= 0; i--) {
+            maxHeapfiy(arr, i, len - 1);
+        }
+        // 2.对堆化数据排序
+        for (int i = len - 1; i > 0; i--) {
+            swap(arr, 0, i);
+            maxHeapfiy(arr, 0, i - 1);
+        }
+    }
+
+    private static void maxHeapfiy(int[] arr, int node, int end) {
+        int left = (node << 1) + 1;
+        int right = left + 1;
+        if (left > end) {
+            return;
+        }
+        int cMax = left;
+        if (right <= end && arr[left] < arr[right]) {
+            cMax = right;
+        }
+        if (arr[node] < arr[cMax]) {
+            swap(arr, cMax, node);
+            maxHeapfiy(arr, cMax, end);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 
 }
