13103f634a5ea8ee590fe6bd3f1c74e61c657233
胡凯波 <1069977596@qq.com>
2019-11-30 14:22:53 +0800

    冒泡和快速排序

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] array1 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
-        int[] array2 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array1 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array2 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
         sort1(array1);
-        sort2(array2);
+        sort2(array2, 0, array2.length - 1);
 
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
@@ -15,9 +15,47 @@ public class Sort {
 
     // 排序算法1
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    //冒泡排序
+    public static void sort1(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 0; j < array.length - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+
+                    array[j] = array[j + 1];
+
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
 
     // 排序算法2
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    //快速排序
+    public static void sort2(int[] array, int left, int right) {
+
+        if (left > right) {
+            return;
+        }
+        int base = array[left];
+        int i = left;
+        int j = right;
+        while (i != j) {
+            while (array[j] >= base && i < j) {
+                j--;
+            }
+            while (array[i] <= base && i < j) {
+                i++;
+            }
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        array[left] = array[i];
+        array[i] = base;
+        sort2(array, left, i - 1);
+        sort2(array, j + 1, right);
+    }
 }
+
