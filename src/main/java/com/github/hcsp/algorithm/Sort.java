ef3bfa381aba3affef8ad2604954bc8e4832040b
shenwl <shenweilun96@gmail.com>
2019-12-16 22:04:35 +0800

    实现冒泡排序

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -22,7 +22,7 @@ public class Sort {
     // 排序算法2
     // 按照从小到大排序
     public static void sort2(int[] array) {
-        quickSort(array, 0, array.length - 1, false);
+        bubbleSort(array, 0, array.length - 1);
     }
 
     public static void swap(int[] array, int i, int j) {
@@ -31,7 +31,8 @@ public class Sort {
         array[j] = temp;
     }
 
-    public static void quickSort(int[] array, int start, int end, boolean asc) {
+    // 快排，支持升序和降序
+    public static void quickSort(int[] array, int start, int end, boolean isAsc) {
         if (end <= start) {
             return;
         }
@@ -39,19 +40,39 @@ public class Sort {
         int low = start, high = end;
 
         while (low < high) {
-            while (low < high && ((pivot <= array[high] && asc) || (pivot >= array[high] && !asc))) {
+            while (low < high && ((pivot <= array[high] && isAsc) || (pivot >= array[high] && !isAsc))) {
                 high--;
             }
             swap(array, low, high);
 
-            while (low < high && ((array[low] <= pivot && asc) || (array[low] >= pivot && !asc))) {
+            while (low < high && ((array[low] <= pivot && isAsc) || (array[low] >= pivot && !isAsc))) {
                 low++;
             }
             swap(array, low, high);
         }
 
-        quickSort(array, start, low, asc);
-        quickSort(array, low + 1, end, asc);
+        quickSort(array, start, low, isAsc);
+        quickSort(array, low + 1, end, isAsc);
 
+    }
+
+    // 冒泡排序
+    public static void bubbleSort(int[] array, int start, int end) {
+        int high = end;
+
+        while (high > start) {
+            int maxIndex = start;
+            int max = array[start];
+            for (int i = maxIndex + 1; i <= high; i++) {
+                int currentEl = array[i];
+                if (max > currentEl) {
+                    swap(array, i, maxIndex);
+                } else {
+                    max = currentEl;
+                }
+                maxIndex = i;
+            }
+            high--;
+        }
     }
 }
