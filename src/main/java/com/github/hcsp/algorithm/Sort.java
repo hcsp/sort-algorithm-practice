b598fe12a2ef1874365852a66decc9a8d48cb1b7
zaker-666 <1075151259china@gmail.com>
2020-01-26 22:32:04 +0800

    finish and push

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -4,9 +4,12 @@ import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] array1 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
-        int[] array2 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array1 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array2 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        long timeStart = System.nanoTime();
         sort1(array1);
+        long timeEnd = System.nanoTime();
+        System.out.println(timeEnd - timeStart);
         sort2(array2);
 
         System.out.println(Arrays.toString(array1));
@@ -15,9 +18,33 @@ public class Sort {
 
     // 排序算法1
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    // 冒泡排序
+    public static void sort1(int[] array) {
+        int temp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
 
     // 排序算法2
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    // 插入排序
+    // 快速排序
+    public static void sort2(int[] array) {
+        int j;
+        for (int i = 1; i < array.length; i++) {
+            Integer temp = array[i];
+            for (j = i; j > 0 && temp.compareTo(array[j - 1]) < 0; j--) {
+                array[j] = array[j - 1];
+            }
+            array[j] = temp;
+        }
+    }
 }
+
