e4a22fcc9428831a31fe361256bb234145ade07e
Maricaya <43136257+Maricaya@users.noreply.github.com>
2020-02-23 16:33:36 +0800

    feat: 完成冒泡&快速排序 (#56)
    feat: 完成冒泡&快速排序 (#56)

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -15,9 +15,53 @@ public class Sort {
 
     // 排序算法1
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    static void sort1(int[] array) {
+        int len = array.length;
+        for (int i = 0; i < len - 1; i++) {
+            boolean finish = true;
+            for (int j = 0; j < len - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    swap(array, j + 1, j);
+                    finish = false;
+                }
+            }
+            if (finish) {
+                break;
+            }
+        }
+    }
 
     // 排序算法2
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    static void sort2(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition(arr, left, right);
+            quickSort(arr, left, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, right);
+        }
+    }
+
+    private static int partition(int[] arr, int left, int right) {
+        // 设定基准值（pivot）
+        int pivot = left;
+        int index = pivot + 1;
+        for (int i = index; i <= right; i++) {
+            if (arr[i] < arr[pivot]) {
+                swap(arr, i, index);
+                index++;
+            }
+        }
+        swap(arr, pivot, index - 1);
+        return index - 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
