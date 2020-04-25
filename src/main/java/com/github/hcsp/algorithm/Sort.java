a7fc7caf9690015fcdb48f7ee71bedf7d56f7de3
arsonalXxu <897386402@qq.com>
2020-04-25 13:50:40 +0800

    sort algorithm (#74)
    sort algorithm (#74)

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -6,18 +6,68 @@ public class Sort {
     public static void main(String[] args) {
         int[] array1 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
         int[] array2 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array3 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
         sort1(array1);
         sort2(array2);
+        sort3(array3);
 
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
+        System.out.println(Arrays.toString(array3));
     }
 
-    // 排序算法1
+    // 排序算法1，冒泡排序
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    public static void sort1(int[] array) {
 
-    // 排序算法2
+        for (int i = 0; i < array.length; i++) {
+            boolean isNumChange = false;
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp;
+                    tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                    isNumChange = true;
+                }
+            }
+            // 没有数据交换提前结束
+            if (!isNumChange) {
+                break;
+            }
+        }
+    }
+
+    // 排序算法2，插入排序
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    public static void sort2(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int value = array[i];
+            int j = i - 1;
+            for (; j >= 0; j--) {
+                if (array[j] > value) {
+                    array[j + 1] = array[j];
+                } else {
+                    break;
+                }
+            }
+            array[j + 1] = value;
+        }
+    }
+
+    // 排序算法3， 选择排序:未排序区间的最小值交换到已排序的尾部
+    // 按照从小到大排序
+    public static void sort3(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < array.length; j++) {
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
+                }
+            }
+            int tmp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = tmp;
+        }
+    }
 }
