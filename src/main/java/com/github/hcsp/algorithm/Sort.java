d3bfd5c20a178250860b87145eadcd60f5b3c758
kongmu373 <kongmu1235@gmail.com>
2020-06-08 13:08:29 +0800

    完成排序算法

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] array1 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
-        int[] array2 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array1 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array2 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
         sort1(array1);
         sort2(array2);
 
@@ -15,9 +15,46 @@ public class Sort {
 
     // 排序算法1
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    public static void sort1(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static void quickSort(int[] array, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int pivot = partition(array, left, right);
+        quickSort(array, left, pivot - 1);
+        quickSort(array, pivot + 1, right);
+    }
+
+    private static int partition(int[] array, int left, int right) {
+        int index = left;
+        for (int i = left; i < right; i++) {
+            if (array[i] < array[right]) {
+                swap(array, i, index++);
+            }
+        }
+        swap(array, index, right);
+        return index;
+    }
+
+    private static void swap(int[] array, int i, int index) {
+        int temp = array[i];
+        array[i] = array[index];
+        array[index] = temp;
+    }
+
 
     // 排序算法2
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    public static void sort2(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    swap(array, j, j + 1);
+                }
+            }
+        }
+    }
 }
