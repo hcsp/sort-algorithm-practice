ffb9b3c88962216535fdb738bb35d7225a2a7f1e
豪 <linesahao@outlook.com>
2020-06-29 15:40:16 +0800

    排序算法练习：只实现了冒泡和快排。 (#89)
    排序算法练习：只实现了冒泡和快排。 (#89)

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -13,11 +13,54 @@ public class Sort {
         System.out.println(Arrays.toString(array2));
     }
 
-    // 排序算法1
+    // 排序算法1 冒泡
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    public static void sort1(int[] array) {
+        int length = array.length;
+        for (int len = length - 1; len > 0; len--) {
+            for (int i = 0; i < length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    // 交换
+                    swapValue(array, i, (i + 1));
+                }
+            }
+        }
 
-    // 排序算法2
+    }
+
+    // 排序算法2 快排
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    public static void sort2(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high]; // 基准值
+        int small = (low - 1);
+        for (int i = low; i <= high - 1; i++) {
+            if (array[i] <= pivot) {
+                small++;
+                swapValue(array, small, i);
+            }
+        }
+        small++;
+        swapValue(array, small, high);
+
+        return small;
+    }
+
+    private static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+    }
+
+    private static void swapValue(int[] array, int small, int i) {
+        int temp = array[small];
+        array[small] = array[i];
+        array[i] = temp;
+    }
+
 }
