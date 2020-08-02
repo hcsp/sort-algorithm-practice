1dd3b48e913fb7f7100376a22cea91c18f82377f
chenxiaolani <2524729719@qq.com>
2020-08-02 23:54:49 +0800

    代码需要格式化

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -32,15 +32,15 @@ public class Sort {
     // 排序算法2 插入
     // 按照从小到大排序
     public static void sort2(int[] a) {
-        int i, j, insertNote;// 要插入的数据
-        for (i = 1; i < a.length; i++) {// 从数组的第二个元素开始循环将数组中的元素插入
-            insertNote = a[i];// 设置数组中的第2个元素为第一次循环要插入的数据
+        int i, j, insertNote; // 要插入的数据
+        for (i = 1; i < a.length; i++) { // 从数组的第二个元素开始循环将数组中的元素插入
+            insertNote = a[i]; // 设置数组中的第2个元素为第一次循环要插入的数据
             j = i - 1;
             while (j >= 0 && insertNote < a[j]) {
-                a[j + 1] = a[j];// 如果要插入的元素小于第j个元素,就将第j个元素向后移动
+                a[j + 1] = a[j]; // 如果要插入的元素小于第j个元素,就将第j个元素向后移动
                 j--;
             }
-            a[j + 1] = insertNote;// 直到要插入的元素不小于第j个元素,将insertNote插入到数组中
+            a[j + 1] = insertNote; // 直到要插入的元素不小于第j个元素,将insertNote插入到数组中
         }
     }
 }
