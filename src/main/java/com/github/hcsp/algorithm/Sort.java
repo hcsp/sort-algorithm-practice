e5cbecb39b1741d174ca791160f602677e3f2b09
luckzhu <luckzhu1992@gmail.com>
2020-09-29 16:47:45 +0800

    排序算法

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] array1 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
-        int[] array2 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array1 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array2 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
         sort1(array1);
         sort2(array2);
 
@@ -15,9 +15,36 @@ public class Sort {
 
     // 排序算法1
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    // 冒泡
+    public static void sort1(int[] array) {
+        int i = 0;
+        while (i <= array.length - 2) {
+            int j = i + 1;
+            while (j <= array.length - 1) {
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+                j++;
+            }
+            i++;
+        }
+    }
 
     // 排序算法2
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    // 插入排序
+    public static void sort2(int[] array) {
+        int i;
+        int j;
+        int len = array.length;
+        for (i = 1; i < len; i++) {
+            int temp = array[i];
+            for (j = i - 1; j >= 0 && array[j] > temp; j--) {
+                array[j + 1] = array[j];
+            }
+            array[j + 1] = temp;
+        }
+    }
 }
