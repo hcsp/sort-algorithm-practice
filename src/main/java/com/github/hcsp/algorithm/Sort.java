01c9dd9fa6ab159ee3cf4f26640268ae2cbb79e4
Su <sgz886@gmail.com>
2020-12-17 22:55:45 +0800

    冒泡排序和选择排序

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -13,11 +13,35 @@ public class Sort {
         System.out.println(Arrays.toString(array2));
     }
 
-    // 排序算法1
+    // 排序算法1 冒泡排序
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    public static void sort1(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            // 2.控制每轮比较几次
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    switchArray(array,j,j+1);
+                }
+            }
+        }
+    }
 
-    // 排序算法2
+    // 排序算法2 选择排序
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    public static void sort2(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            // 2.控制每轮比较几次
+            for (int j = i+1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    switchArray(array,i,j);
+                }
+            }
+        }
+    }
+
+    public static void switchArray(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j]=temp;
+    }
 }
