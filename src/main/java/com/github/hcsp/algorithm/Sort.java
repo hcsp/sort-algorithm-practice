fd43e07e43103890ea2d94b305d4dd427843f7c0
sgz886 <sgz886@gmail.com>
2020-12-17 23:01:07 +0800

    冒泡排序和选择排序 (#106)
    冒泡排序和选择排序 (#106)

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] array1 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
-        int[] array2 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array1 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array2 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
         sort1(array1);
         sort2(array2);
 
@@ -13,11 +13,35 @@ public class Sort {
         System.out.println(Arrays.toString(array2));
     }
 
-    // 排序算法1
+    // 排序算法1 冒泡排序
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    public static void sort1(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            // 2.控制每轮比较几次
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    switchArray(array, j, j + 1);
+                }
+            }
+        }
+    }
 
-    // 排序算法2
+    // 排序算法2 选择排序
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    public static void sort2(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            // 2.控制每轮比较几次
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    switchArray(array, i, j);
+                }
+            }
+        }
+    }
+
+    public static void switchArray(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
