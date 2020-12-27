8b542b2b2cdd4b6c62c4d0e0fe62342e02f11e42
daimon0809 <51164058+daimon0809@users.noreply.github.com>
2020-12-27 21:48:17 +0800

    排序算法 (#109)
    排序算法 (#109)

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] array1 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
-        int[] array2 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array1 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array2 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
         sort1(array1);
         sort2(array2);
 
@@ -15,9 +15,47 @@ public class Sort {
 
     // 排序算法1
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    public static void sort1(int[] array) {
+        int tmp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i; j++) {
+                if (j < array.length - i - 1 && array[j] > array[j + 1]) {
+                    tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+    }
 
     // 排序算法2
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    public static void sort2(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static void quickSort(int[] array, int left, int right) {
+        if (left > right) {
+            return;
+        }
+        int base = array[left];
+        int i = left, j = right;
+        while (i != j) {
+            while (array[j] >= base && i < j) {
+                --j;
+            }
+            while (array[i] <= base && i < j) {
+                ++i;
+            }
+            if (i < j) {
+                int tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+        array[left] = array[i];
+        array[i] = base;
+        quickSort(array, left, i - 1);
+        quickSort(array, i + 1, right);
+    }
 }
