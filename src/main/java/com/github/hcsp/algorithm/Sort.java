5cda4de0942fa076739a2bd15dca1a375cb81b67
catmmao <catmmao@gmail.com>
2021-01-12 15:03:41 +0800

    实现冒泡排序，选择排序

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -15,9 +15,39 @@ public class Sort {
 
     // 排序算法1
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    public static void sort1(int[] array) {
+        int length = array.length;
+
+        for (int i = 0; i <= length - 1; i++) {
+            for (int j = length - 1; j > i; j--) {
+                if (array[j - 1] > array[j]) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+    }
 
     // 排序算法2
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    public static void sort2(int[] array) {
+        int length = array.length;
+
+        for (int i = 0; i < length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j <= length - 1; j++) {
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
 }
