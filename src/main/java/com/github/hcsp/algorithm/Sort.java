0b8951d7df388abb87fe5f373147a13af6e087e8
jianqibot <85292386+jianqibot@users.noreply.github.com>
2021-10-10 23:37:03 +1100

    job done (#123)
    job done (#123)

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -15,9 +15,56 @@ public class Sort {
 
     // 排序算法1
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    public static void sort1(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
 
     // 排序算法2
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    public static void sort2(int[] array) {
+        quickSort(array, 0, array.length-1);
+    }
+
+    private static void quickSort(int[] array, int leftIndex, int rightIndex) {
+        if (leftIndex < rightIndex) {
+            int index = partition(array, leftIndex, rightIndex);
+            quickSort(array, leftIndex, index-1);
+            quickSort(array, index+1, rightIndex);
+        }
+    }
+
+    private static int partition(int[] array, int leftIndex, int rightIndex) {
+        int pivot = array[rightIndex];
+        while (leftIndex < rightIndex) {
+            while (leftIndex < rightIndex && array[leftIndex] < pivot) {
+                leftIndex++;
+            }
+            if (leftIndex < rightIndex) {
+                swap(array, leftIndex, rightIndex);
+                rightIndex--;
+            }
+            while (leftIndex < rightIndex && array[rightIndex] > pivot) {
+                rightIndex--;
+            }
+            if (leftIndex < rightIndex) {
+                swap(array, leftIndex, rightIndex);
+                leftIndex++;
+            }
+        }
+        return leftIndex;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+    }
 }
