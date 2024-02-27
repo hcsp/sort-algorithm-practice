fb1a57c252fcbc8c7df607eb10fe7818f9414582
rf4github <39894049+rf818@users.noreply.github.com>
2024-02-27 21:53:14 +0800

    sort-algorithm-practice (#133)
    sort-algorithm-practice (#133)

--- src/main/java/com/github/hcsp/algorithm/Sort.java
+++ src/main/java/com/github/hcsp/algorithm/Sort.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] array1 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
-        int[] array2 = new int[] {4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array1 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
+        int[] array2 = new int[]{4, 8, 1, 7, 4, 0, 5, 8, 7, 5, 9, 6, 4, 0};
         sort1(array1);
-        sort2(array2);
+        sort3(array2);
 
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
@@ -15,9 +15,51 @@ public class Sort {
 
     // 排序算法1
     // 按照从小到大排序
-    public static void sort1(int[] array) {}
+    public static void sort1(int[] array) {
+        if (array.length < 2 || array == null) {
+            return;
+        }
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                minIndex = array[j] < array[minIndex] ? j : minIndex;
+            }
+            swapArray(array, i, minIndex);
+        }
+    }
+
+    private static void swapArray(int[] array, int i, int minIndex) {
+        if (i == minIndex) {
+            return;
+        }
+        array[i] ^= array[minIndex];
+        array[minIndex] ^= array[i];
+        array[i] ^= array[minIndex];
+    }
 
     // 排序算法2
     // 按照从小到大排序
-    public static void sort2(int[] array) {}
+    public static void sort2(int[] array) {
+        if (array == null || array.length < 2) {
+            return;
+        }
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    swapArray(array, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void sort3(int[] array) {
+        if (array == null || array.length < 2) {
+            return;
+        }
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i - 1; j >= 0 && array[j] > array[j + 1]; j--) {
+                swapArray(array, j, j + 1);
+            }
+        }
+    }
 }
